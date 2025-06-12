db4933aaf7699465a1ce90fcf218c1e3c82d5d29
Ritik Yadav <ritikyadav9405@gmail.com>
2025-06-12 11:11:41 +0530

    Added Extension Method to add +91 before Phone no

---
+++ ConsoleToAPI/ExtensionMethods/ExtendPhoneNo.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleToAPI.Models;
+
+namespace ConsoleToAPI.ExtensionMethods
+{
+    public static class ExtendPhoneNo
+    {
+        public static string AddPhone(this EmployeesModel emp) {
+            if (!emp.Phone_No.StartsWith("+91"))
+            {
+                emp.Phone_No = "+91" + emp.Phone_No;
+                return emp.Phone_No;
+            }
+            return emp.Phone_No;
+        }
+    }
+
+}
