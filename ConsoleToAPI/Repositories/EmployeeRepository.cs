71fe3cd7885e64ff1d524c4e4768090fc829b37a
Ritik Yadav <ritikyadav9405@gmail.com>
2025-06-12 11:08:49 +0530

    Added EmployeeRepository

---
+++ ConsoleToAPI/Repositories/EmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleToAPI.ExtensionMethods;
+using ConsoleToAPI.Models;
+
+namespace ConsoleToAPI.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        List<EmployeesModel> employess = new List<EmployeesModel>();
+        public List<EmployeesModel> GetAll()
+        {
+            return employess;
+        }
+
+        public void insert(EmployeesModel employee)
+        {
+            if (employess.Count == 0) employee.id = 1;
+            else employee.id = employess[employess.Count - 1].id + 1;
+            employee.AddPhone();
+            employess.Add(employee);
+        }
+    }
+}
---
+++ ConsoleToAPI/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleToAPI.Models;
+
+namespace ConsoleToAPI.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        List<EmployeesModel>GetAll();
+        void insert(EmployeesModel employee);
+
+    }
+}
