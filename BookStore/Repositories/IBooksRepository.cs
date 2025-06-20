3af71d18477faf3315ad562f0dbbe33d23d1d28f
Ritik Yadav <ritikyadav9405@gmail.com>
2025-06-20 14:17:46 +0530

    Initial commit: Added Book Store Web API project

---
+++ BookStore/Repositories/IBooksRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.Models;
+
+namespace BookStore.Repositories
+{
+    public interface IBooksRepository
+    {
+          Task<BookModel> AddBook(BookModel Book);
+          Task<List<BookModel>> GetBooks();
+          Task<BookModel> GetBookById(int id);
+
+    }
+}
